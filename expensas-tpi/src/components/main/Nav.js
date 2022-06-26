27ed75c8720c9d672c1e6ba553750b35d42759ab
lucapec <lucapecorelli1@gmail.com>
2022-06-26 12:22:17 -0300

    Login funcionando, habria uqe ver el estilizado

--- expensas-tpi/src/components/main/Nav.js
+++ expensas-tpi/src/components/main/Nav.js
@@ -1,16 +1,30 @@
 import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { LoginButton } from "./Login";
+import { LogoutButton } from "./Logout";
+import { Profile } from "./Profile";
+
 import Lightswitch from "./Lightswitch";
+
 import './Container.css';
 import './Nav.css'
+
 const Nav = () => {
+  const {isAuthenticated} = useAuth0();
   return (
     <div>
       <nav className="nav-bar">
         <h1>AE</h1>
         
         <div className="buttons">
-          <button>INICIAR SESION</button>
-          <button>REGISTRARSE</button>
+        {isAuthenticated ? (
+          <>
+            <Profile />
+            <LogoutButton />
+          </>
+        ) : (
+          <LoginButton />
+        )}
         <Lightswitch />
         </div>
       </nav>
--- expensas-tpi/src/index.js
+++ expensas-tpi/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Auth0Provider} from '@auth0/auth0-react'
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Auth0Provider domain='dev-sdbwkr53.us.auth0.com' clientId='s4KQOEeCTi90v0xNmGjolqsHLwIBxxrb' redirectUri={window.location.origin}>
+           <App />
+    </Auth0Provider>
+    
   
 );
 
